24e17a02b5ba663677e7257d7942d6eed5f37b4a
Gabriel Maco <21101344@ue.edu.pe>
2023-05-13 21:29:33 -0500

    Agregar archivos de proyecto.

---
+++ DPA.SHOPPING.DOMAIN/Core/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using DPA.SHOPPING.DOMAIN.Core.Entities;
+
+namespace DPA.SHOPPING.DOMAIN.Core.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<bool> Delete(int id);
+        Task<IEnumerable<Category>> GetAll();
+        Task<Category> GetById(int id);
+        Task<bool> Insert(Category category);
+        Task<bool> Update(Category category);
+    }
+}
